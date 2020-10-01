27e6b755cf6640dcd4e7d712fcc1559d8076b478
Aleksey <al.nkow@gmail.com>
2020-10-01 21:38:03 +0300

    test

--- src/components/DlMap.jsx
+++ src/components/DlMap.jsx
@@ -24,22 +24,33 @@ const TestBtn = styled.div`
   }
 `;
 
+
+
+
+const AREA_SIZE = 500;
+
 const DlMap = () => {
   console.log('++++++++++ RENDER +++++++++++');
 
+  // ymaps.coordSystem.geo.getDistance(moscowCoords, newYorkCoords)
+
 
   let instMap = '';
+  let entranceRef = '';
 
   const [pointCoords, setPointCoords] = useState([]);
   const [entranceCoords, setEntranceCoords] = useState([]);
   const [showArea, setShowArea] = useState(false);
+  const [ymaps, setYmaps] = useState(null);
 
-  const onLoadMap = (ymaps) => {
+  const onLoadMap = (map) => {
     // const location = window.ymaps.geolocation.get(
     //   { mapStateAutoApply: true },
     // )
     
-    console.log('MAP LOADED >>>>>', ymaps, instMap);
+    console.log('MAP LOADED >>>>>', map);
+    setYmaps(map);
+    // ymaps.coordSystem.geo.getDistance(moscowCoords, newYorkCoords)
 
 
 
@@ -48,11 +59,10 @@ const DlMap = () => {
 
 
     // ловим клик по карте
-    instMap.events.add('click', (e) => {
-      console.log('KAKOGO HERA?? >>>>>>>>', showArea);
-      // const coords = e.get('coords');
-      // setPointCoords(coords);
-    });
+    // instMap.events.add('click', (e) => {
+    //   const coords = e.get('coords');
+    //   setPointCoords(coords);
+    // });
 
 
 
@@ -101,6 +111,27 @@ const DlMap = () => {
     if (showArea) setEntranceCoords([]);
   }
 
+
+
+
+
+  const mapClick = e => {
+    if (!showArea) {
+      const coords = e.get('coords');
+      setPointCoords(coords);
+    }
+  }
+
+  const moveEntrance = () => {
+    const newCoords = entranceRef.geometry.getCoordinates();
+    const distance = ymaps.coordSystem.geo.getDistance(newCoords, pointCoords);
+    if (distance > AREA_SIZE) {
+      setEntranceCoords([]);
+    } else {
+      setEntranceCoords(newCoords);
+    }
+  }
+
   return (
     <YMaps query={{ apikey: 'fd1cea5d-2179-4ca3-948a-55b839aa8c79' }}>
       <TestBtn onClick={SetTestCoords}>Установить координаты</TestBtn>
@@ -109,8 +140,9 @@ const DlMap = () => {
       <div>{entranceCoords[0]} - {entranceCoords[1]}</div>
       <MapWrap>
         <Map
-          modules={["geolocation", "geocode"]}
+          modules={['geolocation', 'geocode', 'coordSystem.geo']}
           onLoad={(inst) => onLoadMap(inst)}
+          onClick={mapClick}
           width={'100%'}
           height={'500px'}
           defaultState={{ center: [55.75, 37.57], zoom: 9 }}
@@ -141,7 +173,7 @@ const DlMap = () => {
           {
             pointCoords && pointCoords.length && showArea ? (
               <Circle
-                geometry={[pointCoords, 500]}
+                geometry={[pointCoords, AREA_SIZE]}
                 options={{
                   draggable: false,
                   fillColor: '#FFD2D2',
@@ -166,23 +198,17 @@ const DlMap = () => {
               <Placemark
                 modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
                 geometry={entranceCoords}
+                onDragend={moveEntrance}
                 options={{
                   draggable: true,
-                  // preset: 'islands#dotIcon',
+                  preset: 'islands#dotIcon',
                   iconColor: '#ef2c2c',
                 }}
                 properties={{
                   hintContent: 'Уточнение подъезда',
                   balloonContent: 'Это балун подъезда',
                 }}
-                instanceRef={ref => {
-                  if (ref) {
-                    ref.events.add('dragend', function(e) {
-                      const newCoords = ref.geometry.getCoordinates();
-                      setEntranceCoords(newCoords);
-                    });
-                  }
-                }}
+                instanceRef={ref => (entranceRef = ref)}
               />
             ) : ''
           }

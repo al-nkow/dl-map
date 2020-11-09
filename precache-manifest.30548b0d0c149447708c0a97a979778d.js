1900f39466568de11dec0ebbf05f243ca0ad09cc
Aleksey <al.nkow@gmail.com>
2020-11-09 12:09:55 +0300

    Updates

---
+++ precache-manifest.30548b0d0c149447708c0a97a979778d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeca97a2f2cf65b2c8fbe50fb5611fc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f55c6b3ee8609028b086",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f42b983caafbae88406",
+    "url": "/static/js/2.c64cd692.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c64cd692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55c6b3ee8609028b086",
+    "url": "/static/js/main.ab05f4c9.chunk.js"
+  },
+  {
+    "revision": "efbfed7db62711045cad",
+    "url": "/static/js/runtime-main.c95577ef.js"
+  }
+]);
